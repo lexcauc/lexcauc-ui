cf59d77d77489c996f1d784bb82656392ea6988b
Oleg Belyaev <obelyaev@gmail.com>
2019-09-21 16:09:39 +0300

    added lingtypology library and languages column types

--- shared.R
+++ shared.R
@@ -1,15 +1,16 @@
-# Read CSV database files (either locally or globally)
-
-read.csv("https://raw.githubusercontent.com/lexcauc/lexcauc-db/master/lexcauc-concepts.csv",stringsAsFactors = FALSE) -> concepts
-read.csv("https://raw.githubusercontent.com/lexcauc/lexcauc-db/master/forms.csv",stringsAsFactors = FALSE) -> forms
-read.csv("https://raw.githubusercontent.com/lexcauc/lexcauc-db/master/languages.csv",stringsAsFactors = FALSE) -> langs
-#read.csv("../lexcauc-db/lexcauc-concepts.csv",stringsAsFactors = FALSE) -> concepts
-#read.csv("../lexcauc-db/forms.csv",stringsAsFactors = FALSE) -> forms
-#read.csv("../lexcauc-db/languages.csv",stringsAsFactors = FALSE) -> langs
-
 # Common libraries and knitr settings
 
+library(lingtypology)
 library(DT)
 library(tidyverse)
 library(dplyr)
 knitr::opts_chunk$set(echo=FALSE, message=FALSE)
+
+# Read CSV database files (either locally or globally)
+
+read.csv("https://raw.githubusercontent.com/lexcauc/lexcauc-db/master/lexcauc-concepts.csv", stringsAsFactors = FALSE) -> concepts
+read.csv("https://raw.githubusercontent.com/lexcauc/lexcauc-db/master/forms.csv", stringsAsFactors = FALSE) -> forms
+read_csv("https://raw.githubusercontent.com/lexcauc/lexcauc-db/master/languages.csv", col_types = "cccfdd") -> langs
+#read.csv("../lexcauc-db/lexcauc-concepts.csv",stringsAsFactors = FALSE) -> concepts
+#read.csv("../lexcauc-db/forms.csv",stringsAsFactors = FALSE) -> forms
+#read_csv("../lexcauc-db/languages.csv",stringsAsFactors = FALSE, col_types = "cccfdd") -> langs
